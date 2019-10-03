14a517715a832fcdf7416a84c8ba085f198ebed9
Daniel Hernandez <danielmark252525@gmail.com>
2019-10-03 14:14:02 -0400

    Test

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI: "mongodb+srv://user:zEe0cFiZaiF3miev@cluster0-muuon.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "f052535-edfg6-1574859-dfghfxfg4-2a2dhxhrhce4"
-};
